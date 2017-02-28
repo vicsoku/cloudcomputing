457676beb6c7504a16cc0d2dfd9cba175bed9d8f
Victor Enrique Solis Kuh <vesk3@itl197.student.eecs.qmul.ac.uk>
2017-02-28 12:58:01 +0000

    my initial deployment

--- wsgi/app/views.py
+++ wsgi/app/views.py
@@ -1,6 +1,23 @@
-from app import app
+from flask import Flask, jsonify
 
-@app.route('/')
-@app.route('/index')
-def index():
-	return "Hello, World!"
+app=Flask(__name__)
+
+mytasks=[
+{
+	'id':1,
+	'task':u'FInish Hello World',
+	'session': u'Week 7',
+	'done': True
+},
+{
+	'id': 2,
+	'task': u'Finish first API',
+	'session': u'Week 7',
+	'done': False
+}
+]
+
+@app.route('/todo/api/task',methods=['GET'])
+def get_tasks():
+	return jsonify({'tasks':mytasks})
+
