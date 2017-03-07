89e86398d00a09f275942d23de27db73947a9032
Victor Enrique Solis Kuh <vesk3@itl195.student.eecs.qmul.ac.uk>
2017-03-07 11:52:24 +0000

    my initial deployment

--- wsgi/run.py
+++ wsgi/run.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import Flask
+#from flask import Flask
 if __name__=="__main__":
 	app.run(debug = False)
 #Note: the debug MUST BE set to false in production for security
