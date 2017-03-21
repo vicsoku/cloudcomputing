8d26b35605466ac538bb6903a4fb56440e79fcfe
Victor Enrique Solis Kuh <vesk3@itl119.student.eecs.qmul.ac.uk>
2017-03-21 13:16:41 +0000

    arman

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='YourAppName',
       author='Your Name',
       author_email='example@example.com',
       url='https://www.python.org/community/sigs/current/distutils-sig',
-      install_requires=['Flask','flask-wtf', 'unirest', 'wtforms','NumPy', 'Six', 'Jinja2', 'Requests', 'Tornado >= 4.0', 'PyYaml', 'DateUtil', 'bokeh==0.12.4', 'Futures', 'Flask-PyMongo'],
+      install_requires=['Flask','flask-wtf', 'unirest', 'wtforms', 'bokeh==0.12.4', 'Flask-PyMongo'],
       )
